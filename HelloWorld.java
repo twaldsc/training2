cd154fb170cc657ecf6a51e8252c097fb061a928
Timo Waldschmidt <timo.waldschmidt@gmx.de>
2023-04-18 22:48:27 +0200

    Feature-1 Commit 01

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
 
+
+    public static void featureWork() {
+        System.out.println("New Feature-1");
+    }
+
     public static void main(String[] args) {
         System.out.println("Init commit");
     }
