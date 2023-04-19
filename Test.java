47477db06c89e767a6c774e7a07bc4e424128f7a
Timo Waldschmidt <timo.waldschmidt@gmx.de>
2023-04-19 21:02:19 +0200

    Feature-Commit-1

--- Test.java
+++ Test.java
@@ -1,4 +1,9 @@
 public class Test {
+
+    public void feature_x () {
+        System.out.println("Feature_x");
+    }
+
     public static void main(String[] args) {
         System.out.println("Main-Test");
 
