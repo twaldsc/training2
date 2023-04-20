0477981b358a8151f0e6d7e990956cf9816883ad
Timo Waldschmidt <timo.waldschmidt@gmx.de>
2023-04-20 17:42:11 +0200

    Master-Commit for merge-test

--- Test.java
+++ Test.java
@@ -1,4 +1,9 @@
 public class Test {
+
+    public void merge() {
+        System.out.println("Merge to Main");
+    }
+
     public static void main(String[] args) {
         System.out.println("Main-Test");
         System.out.println("Bugfix-Master");
